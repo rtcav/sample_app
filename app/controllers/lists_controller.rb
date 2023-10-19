f6638ddeb6753e4f6df7d8f626bba6d650157cb0
EC2 Default User <ec2-user@ip-172-31-15-162.ap-northeast-1.compute.internal>
2023-10-19 04:40:44 +0000

    [Finish] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,10 +10,11 @@ class ListsController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
@@ -21,8 +22,14 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
 
   private #書き忘れた
   #ストロングパラメータ その下のアクションに影響を与えるため最後に記述
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create' #データを保存するためのルーティング
   get 'lists' => 'lists#index'
   # .../lists/1や.../lists/3に該当する
-  get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
